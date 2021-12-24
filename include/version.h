883f41bc6bea8634e019048f3ad38f92c5b492bd
ermiry <erick.salas@ermiry.com>
2021-12-23 23:12:43 -0600

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
 
 #define SILVERSE_VERSION			"0.1"
 #define SILVERSE_VERSION_NAME		"Version 0.1"
-#define SILVERSE_VERSION_DATE		"17/07/2021"
-#define SILVERSE_VERSION_TIME		"17:03 CST"
+#define SILVERSE_VERSION_DATE		"23/12/2021"
+#define SILVERSE_VERSION_TIME		"23:12 CST"
 #define SILVERSE_VERSION_AUTHOR		"Erick Salas"
 
 // print full silverse version information
